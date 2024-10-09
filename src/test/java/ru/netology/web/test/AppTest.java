434629e2f441eef7c18b4afe7f263f7ed6398b86
LubovKrasheninnikova <altukhovalv@yandex.ru>
2024-10-09 12:07:34 +0300

    add Allure

--- src/test/java/ru/netology/web/test/AppTest.java
+++ src/test/java/ru/netology/web/test/AppTest.java
@@ -1,6 +1,10 @@
 package ru.netology.web.test;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.web.data.Generator;
@@ -12,6 +16,16 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class AppTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @Test
     void letMeTest () {
